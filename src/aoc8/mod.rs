35a5052f141aa84a3f17bf168f2f1e9e7bc7d26b
benni043 <benni.huff@gmail.com>
2023-12-12 15:52:35 +0100

    day8 part2

--- src/aoc8/mod.rs
+++ src/aoc8/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs;
 
-pub fn aoc8() -> Option<i32> {
+pub fn aoc8() -> Option<i64> {
     let result = fs::read_to_string("assets/aoc8/aoc8.txt");
 
     match result {
@@ -33,31 +33,70 @@ pub fn aoc8() -> Option<i32> {
                 }
             }
 
-            let mut pos = "AAA".to_string();
-            let mut cnt = 0;
+            let mut start_postions: Vec<String> = vec![];
+            let mut cnt_for_postitions: Vec<i64> = vec![];
 
-            'outer: loop {
-                for instruction in &left_right_instructs {
-                    if instruction == &'R' {
-                        pos = positions.get(&pos.to_string()).unwrap().right.clone();
-                    } else {
-                        pos = positions.get(&pos.to_string()).unwrap().left.clone();
-                    }
-
-                    cnt += 1;
-
-                    if pos == "ZZZ" {
-                        break 'outer;
-                    }
+            for key in positions.keys() {
+                if key.ends_with("A") {
+                    start_postions.push(key.clone());
                 }
             }
 
-            Some(cnt)
+            for mut start_pos in start_postions {
+                let mut cnt = 0;
+
+                'outer: loop {
+                    for instruction in &left_right_instructs {
+                        if instruction == &'R' {
+                            start_pos =
+                                positions.get(&start_pos.to_string()).unwrap().right.clone();
+                        } else {
+                            start_pos = positions.get(&start_pos.to_string()).unwrap().left.clone();
+                        }
+
+                        cnt += 1;
+
+                        if start_pos.ends_with("Z") {
+                            break 'outer;
+                        }
+                    }
+                }
+
+                cnt_for_postitions.push(cnt);
+            }
+
+            Some(lcm_multiple(cnt_for_postitions))
         }
         Err(err) => {
             println!("{}", err);
             None
         }
+    }
+}
+
+fn gcd(mut a: i64, mut b: i64) -> i64 {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a * b / gcd(a, b)
+}
+
+fn lcm_multiple(mut numbers: Vec<i64>) -> i64 {
+    match numbers.pop() {
+        Some(last) => {
+            let mut result = last;
+            while let Some(num) = numbers.pop() {
+                result = lcm(result, num);
+            }
+            result
+        }
+        None => 1,
     }
 }
 
