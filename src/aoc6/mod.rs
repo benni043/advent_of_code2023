b06d4e22d7b2ddcf597881c681dedf21a929225b
benni043 <benni.huff@gmail.com>
2023-12-12 15:47:44 +0100

    day6 part2

--- src/aoc6/mod.rs
+++ src/aoc6/mod.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-pub fn aoc6() -> Option<i32> {
+pub fn aoc6() -> Option<i64> {
     let result = fs::read_to_string("assets/aoc6/aoc6.txt");
 
     match result {
         Ok(file) => {
-            let mut times: Vec<i32> = vec![];
-            let mut distances: Vec<i32> = vec![];
+            let mut times: Vec<i64> = vec![];
+            let mut distances: Vec<i64> = vec![];
 
             for (index,line) in file.lines().enumerate() {
                 let values: &str = line.split(":").skip(1).next().unwrap().trim();
@@ -16,8 +16,21 @@ pub fn aoc6() -> Option<i32> {
                 } else {
                     distances = values.split_whitespace().filter_map(|value| value.parse().ok()).collect();
                 }
-
             }
+
+            let combined_times: String = times.iter()
+                .map(|&num| num.to_string())
+                .collect();
+
+            let combined_distances: String = distances.iter()
+                .map(|&num| num.to_string())
+                .collect();
+
+            times.clear();
+            times.push(combined_times.parse().unwrap());
+
+            distances.clear();
+            distances.push(combined_distances.parse().unwrap());
 
             let mut sum = 1;
 
