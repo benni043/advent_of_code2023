fb8f6e2bda546a5722d8f52d15f49c4d2d18e3cc
benni043 <benni.huff@gmail.com>
2023-12-12 15:21:59 +0100

    day1 part2

--- src/aoc1/mod.rs
+++ src/aoc1/mod.rs
@@ -1,30 +1,55 @@
 use std::fs;
 
 pub fn aoc1() -> Option<i32> {
+    let str_nums = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
     let result = fs::read_to_string("assets/aoc1/aoc1.txt");
+
     let mut sum = 0;
 
     match result {
         Ok(file) => {
             for line in file.lines() {
-                let my_chars: Vec<_> = line.chars().collect();
-
                 let mut num1 = 0;
+                let mut act_line = line;
 
-                for x in &my_chars {
-                    if x.is_numeric() {
-                        num1 = x.to_string().parse().unwrap();
-                        break;
+                'outer: while !act_line.is_empty() {
+                    for x in &str_nums {
+                        if act_line.starts_with(x) {
+                            num1 = get_numeric_value(x).unwrap_or(0);
+                            break 'outer;
+                        }
                     }
+
+                    for i in 0..10 {
+                        if act_line.starts_with(i.to_string().as_str()) {
+                            num1 = i;
+                            break 'outer;
+                        }
+                    }
+
+                    act_line = &act_line[1..act_line.len()];
                 }
 
                 let mut num2 = 0;
+                let mut act_line = line;
 
-                for x in my_chars.iter().rev() {
-                    if x.is_numeric() {
-                        num2 = x.to_string().parse().unwrap();
-                        break;
+                'outer: while !act_line.is_empty() {
+                    for x in &str_nums {
+                        if act_line.ends_with(x) {
+                            num2 = get_numeric_value(x).unwrap_or(0);
+                            break 'outer;
+                        }
                     }
+
+                    for i in 0..10 {
+                        if act_line.ends_with(i.to_string().as_str()) {
+                            num2 = i;
+                            break 'outer;
+                        }
+                    }
+
+                    act_line = &act_line[0..act_line.len()-1];
                 }
 
                 let new_num = format!("{num1}{num2}");
@@ -38,5 +63,20 @@ pub fn aoc1() -> Option<i32> {
             println!("{}", err);
             None
         }
+    }
+}
+
+fn get_numeric_value(input: &str) -> Option<i32> {
+    match input {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
     }
 }
