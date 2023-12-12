252b1270251ccafe123f002a2434bc99a2b39aed
benni043 <benni.huff@gmail.com>
2023-12-12 15:33:37 +0100

    day4 part1

--- src/aoc4/mod.rs
+++ src/aoc4/mod.rs
@@ -1,29 +1,14 @@
-use std::collections::{BTreeMap, HashMap};
 use std::fs;
 
 pub fn aoc4() -> Option<i32> {
     let result = fs::read_to_string("assets/aoc4/aoc4.txt");
-    let mut card_count: i32 = 0;
+    let mut sum: i32 = 0;
 
     match result {
         Ok(file) => {
-            let mut map: BTreeMap<i32, i32> = BTreeMap::new();
-
             for line in file.lines() {
-                let mut card_and_nums = line.split(":");
-
-                let id: i32 = card_and_nums
-                    .next()
-                    .unwrap()
-                    .trim()
-                    .split_whitespace()
-                    .last()
-                    .unwrap()
-                    .parse()
-                    .unwrap();
-
-                // println!("{}", id);
-                let nums = card_and_nums.next().unwrap().trim();
+                let card_and_nums = line.split(":");
+                let nums = card_and_nums.skip(1).next().unwrap().trim();
 
                 let mut winning_cards_and_my_cards = nums.split("|");
                 let winning_cards = winning_cards_and_my_cards.next().unwrap().trim();
@@ -46,30 +31,13 @@ pub fn aoc4() -> Option<i32> {
                     .filter(|&x| my_numbers.contains(x))
                     .count() as i32;
 
-                if !map.contains_key(&id) {
-                    map.insert(id, 1);
-                } else {
-                    map.insert(id, map.get(&id).unwrap() + 1);
+                if count != 0 {
+                    sum += multiply(count);
                 }
 
-                if map.contains_key(&id) {
-                    let cnt = *map.get(&id).unwrap();
-
-                    for x in id + 1..(id + 1 + count) {
-                        if !map.contains_key(&x) {
-                            map.insert(x, cnt);
-                        } else {
-                            map.insert(x, map.get(&x).unwrap() + cnt);
-                        }
-                    }
-                }
             }
 
-            for x in map.values() {
-                card_count += x;
-            }
-
-            Some(card_count)
+            Some(sum)
         }
         Err(err) => {
             println!("{}", err);
@@ -81,7 +49,7 @@ pub fn aoc4() -> Option<i32> {
 fn multiply(count: i32) -> i32 {
     let mut sum: i32 = 1;
 
-    for x in 0..count - 1 {
+    for x in 0..count-1 {
         sum = sum * 2;
     }
 
