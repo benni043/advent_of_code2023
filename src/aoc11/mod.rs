d19351df7254439397d9464a6f6f50c96d286da9
benni043 <benni.huff@gmail.com>
2023-12-13 21:14:48 +0100

    day11 stuff

--- src/aoc11/mod.rs
+++ src/aoc11/mod.rs
@@ -28,27 +28,27 @@ pub fn aoc11() -> Option<usize> {
                 empty_cols.push(index as i32);
             }
 
-            for row in empty_rows.iter().rev() {
-                grid.insert(*row as usize, ".".repeat(grid[0].len()).chars().collect());
-            }
-
-            for col in empty_cols.iter().rev() {
-                for i in (0..grid.len()).rev() {
-                    let mut chars: Vec<char> = grid.get(i).unwrap().clone();
-                    chars.insert(*col as usize, '.');
-
-                    grid.remove(i);
-                    grid.push(chars);
-                }
-            }
-
-            grid.reverse();
+            // for row in empty_rows.iter().rev() {
+            //     grid.insert(*row as usize, ".".repeat(grid[0].len()).chars().collect());
+            // }
+            //
+            // for col in empty_cols.iter().rev() {
+            //     for i in (0..grid.len()).rev() {
+            //         let mut chars: Vec<char> = grid.get(i).unwrap().clone();
+            //         chars.insert(*col as usize, '.');
+            //
+            //         grid.remove(i);
+            //         grid.push(chars);
+            //     }
+            // }
+            //
+            // grid.reverse();
 
             let mut cords: Vec<Cord> = vec![];
             for (index, row) in grid.iter().enumerate() {
                 for (index2, cell) in row.iter().enumerate() {
                     if cell == &'#' {
-                        cords.push((Cord { x: index2, y: index }));
+                        cords.push((Cord { x: index2, y: index, x_shifts: 0, y_shifts: 0 }));
                     }
                 }
             }
@@ -64,22 +64,44 @@ pub fn aoc11() -> Option<usize> {
                 }
             }
 
-            // println!("{:?}", connections.len());
-
             let mut sum = 0;
             for (from, to) in connections {
                 let mut part1 = 0;
                 if from.x > to.x {
                     part1 = from.x - to.x;
+
+                    for row in empty_rows.clone() {
+                        if to.x >= row as usize && from.x <= row as usize {
+                            sum += 1;
+                        }
+                    }
                 } else {
                     part1 = to.x - from.x;
+
+                    for row in empty_rows.clone() {
+                        if from.x >= row as usize && to.x <= row as usize {
+                            sum += 1;
+                        }
+                    }
                 }
 
                 let mut part2 = 0;
                 if from.y > to.y {
                     part2 = from.y - to.y;
+
+                    for col in empty_cols.clone() {
+                        if to.y >= col as usize && from.y <= col as usize {
+                            sum += 1;
+                        }
+                    }
                 } else {
                     part2 = to.y - from.y;
+
+                    for col in empty_cols.clone() {
+                        if to.y >= col as usize && from.y <= col as usize {
+                            sum += 1;
+                        }
+                    }
                 }
 
                 sum += part1 + part2;
@@ -98,4 +120,6 @@ pub fn aoc11() -> Option<usize> {
 struct Cord {
     x: usize,
     y: usize,
+    x_shifts: usize,
+    y_shifts: usize,
 }
